4241fd29e51a405be90c42a7d1149b8b53cee7e8
Dmytro Nahornyi <d3049999@gmail.com>
2023-03-25 09:24:16 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FeedbackOptions from './Feedback/FeedbackOptions';
 import Notification from './Feedback/Notification';
 import Statistics from './Feedback/Statistics';
 import { FeedbackContainer } from './Feedback/Feedback.styled';
 
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+const App = () => {
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  handleFeedback = option => {
-    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
-  };
+  const handleFeedback = (option) =>
+  setFeedback((prevState) => ({ ...prevState, [option]: prevState[option] + 1 }));
+  
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positivePercentage =
+    totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
-    return total > 0 ? Math.round((good / total) * 100) : 0;
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-
-    return (
-      <FeedbackContainer>
-        <h1>Please leave feedback</h1>
-        <FeedbackOptions
-          options={Object.keys(this.state)}
-          onLeaveFeedback={this.handleFeedback}
-          colors={['green', 'gray', 'red']}
+  return (
+    <FeedbackContainer>
+      <h1>Please leave feedback</h1>
+      <FeedbackOptions
+        options={Object.keys(feedback)}
+        onLeaveFeedback={handleFeedback}
+        colors={['green', 'gray', 'red']}
+      />
+      {totalFeedback > 0 ? (
+        <Statistics
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={totalFeedback}
+          positivePercentage={positivePercentage}
         />
-        {totalFeedback > 0 ? (
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={totalFeedback}
-            positivePercentage={positivePercentage}
-          />
-        ) : (
-          <Notification message="There is no feedback" />
-        )}
-      </FeedbackContainer>
-    );
-  }
-}
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
+    </FeedbackContainer>
+  );
+};
 
 export default App;
-
